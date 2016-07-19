14fe4601deaf3b13563ac0c761646084828b994c
wujie <wujie@tenda.cn>
2016-07-19 10:03:26 +0800

    学习了使用compose还有lift

--- app/src/main/java/com/wujie/rxandroidlearn/MainActivity.java
+++ app/src/main/java/com/wujie/rxandroidlearn/MainActivity.java
@@ -1,8 +1,10 @@
 package com.wujie.rxandroidlearn;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import java.io.File;
@@ -13,17 +15,31 @@ import rx.Observable;
 import rx.Observer;
 import rx.Producer;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imageCollectorView;
+    ImageView imageView;
     InetAddress ip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        int ids = 100;
+        final int id = ids;
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("id", id+"");
+            }
+        });
         ArrayList<File> folders = new ArrayList<>();
+
 //        Observable.from(folders)
 //                .flatMap(new Func1<File, Observable<File>>() {
 //                    @Override
@@ -117,13 +133,179 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         String [] s = {"Heo", "Hiaaa", "dddd"};
-        Observable observable2 = Observable.from(s);
-        observable2.subscribe(subscriber);
+//        Observable observable2 = Observable.from(s);
+//        observable2.subscribe();
+        Observable.from(s).subscribe(new Action1<String>() {
+            @Override
+
+            public void call(String s) {
+                Log.d("sssss", s);
+            }
+        });
         //observable.subscribe(observer);
-        observable.subscribe(subscriber);
-        observable1.subscribe(observer);
+       // observable.subscribe(subscriber);
+       // observable1.subscribe(observer);
+
+        Observable.just(1,2,3,4)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        Log.d("tag", "number:"+integer);
+                    }
+                });
+        Integer [] ints = {1,2,3,4,5};
+        Observable.from(ints)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        Log.e("tag", "numbersss:" + integer);
+                    }
+                });
+        imageView = (ImageView) findViewById(R.id.image_a);
+//        Observable.create(new Observable.OnSubscribe<Drawable>() {
+//
+//            @Override
+//            public void call(Subscriber<? super Drawable> subscriber) {
+//                Drawable drawable = getResources().getDrawable(R.mipmap.ic_launcher);
+//                subscriber.onNext(drawable);
+//                subscriber.onCompleted();
+//            }
+//        }).subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+////                .subscribe(new Action1<Drawable>() {
+////                    @Override
+////                    public void call(Drawable drawable) {
+////                        imageView.setImageDrawable(drawable);
+////                    }
+////                });
+//        .subscribe(new Observer<Drawable>() {
+//            @Override
+//            public void onCompleted() {
+//
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(Drawable drawable) {
+//                imageView.setImageDrawable(drawable);
+//            }
+//        });
+    Observable.just(R.mipmap.ic_launcher)
+            .map(new Func1<Integer, Drawable>() {
+        @Override
+        public Drawable call(Integer integer) {
+            return getResources().getDrawable(integer);
+        }
+    })
+            .subscribe(new Action1<Drawable>() {
+        @Override
+        public void call(Drawable drawable) {
+            imageView.setImageDrawable(drawable);
+        }
+    });
+    Student[] students =null;
+
+        Action1<String> onNextAction = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Log.d("onNextAction", "onNextAction2");
+            }
+        };
+        Action1<Throwable> onErrorAction = new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                Log.e("onErrorAction", throwable.getMessage());
+            }
+        };
+
+        Action0 onCompletedAction = new Action0() {
+            @Override
+            public void call() {
+                Log.d("onCompletedAction", "completed");
+            }
+        };
+
+        Observable<String> observable2 = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("ddddd");
+                subscriber.onError(new Throwable());
+                subscriber.onCompleted();
+            }
+        });
+        observable2.subscribe(onNextAction);
+        observable2.subscribe(onNextAction, onErrorAction);
+        observable2.subscribe(onNextAction, onErrorAction, onCompletedAction);
+
+
+
+
 
     }
+
+    class  mTranstram implements Observable.Transformer<Integer, String> {
+
+        @Override
+        public Observable<String> call(Observable<Integer> integerObservable) {
+            return null;
+        }
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        Log.d("ssss", "Destroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("ssss", "Resume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("ssss", "Restart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("ssss", "start");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("ssss", "Pasuse");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("ssss", "stop");
+        super.onStop();
+    }
+
+    @Override
+    public void finish() {
+        Log.d("ssss", "finnish");
+        super.finish();
+    }
+
+    class Student {
+
+    }
+
 }
---
+++ proguard-rules.pro
@@ -0,0 +1,46 @@
+# Add project specific ProGuard rules here.
+# By default, the flags in this file are appended to flags specified
+# in E:\android\adt-bundle-windows-x86_64-20140702\sdk/tools/proguard/proguard-android.txt
+# You can edit the include path and order by changing the proguardFiles
+# directive in build.gradle.
+#
+# For more details, see
+#   http://developer.android.com/guide/developing/tools/proguard.html
+
+# Add any project specific keep options here:
+
+# If your project uses WebView with JS, uncomment the following
+# and specify the fully qualified class name to the JavaScript interface
+# class:
+#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
+#   public *;
+#}
+
+-dontwarn sun.misc.Unsafe
+-dontwarn org.w3c.dom.bootstrap.DOMImplementationRegistry
+
+-keep public class * extends android.app.Service
+-keep public class * extends android.content.BroadcastReceiver
+
+
+
+
+
+
+# If your project uses WebView with JS, uncomment the following
+# and specify the fully qualified class name to the JavaScript interface
+# class:
+#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
+#   public *;
+#}
+
+
+
+-keepclassmembers class * {
+   public <init>(org.json.JSONObject);
+}
+
+-keepclassmembers enum * {
+    public static **[] values();
+    public static ** valueOf(java.lang.String);
+}
